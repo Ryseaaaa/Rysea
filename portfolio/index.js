d71a5b63e14347a5a8d3c1d55056659f79e9ad44
Ryseaaaa <95868300+Ryseaaaa@users.noreply.github.com>
2025-11-13 18:01:38 +0100

    about me short animation text thingy mabonb

--- portfolio/index.js
+++ portfolio/index.js
@@ -2,6 +2,11 @@ var images = new Array();
 images[0] = "assets/mika.png";
 var x = 0;
 
+var i = 0;
+var txt =
+  "> Ik ben een gedreven software developer die een beetje van alles kent. Altijd nieuwsgierig om meer te leren. Grote interesse in game development";
+var speed = 15;
+
 function changeImage() {
   document.getElementById("photo").src = images[x];
   x += 1;
@@ -9,7 +14,30 @@ function changeImage() {
 }
 
 window.onload = function () {
+  typeWriter();
   setInterval(function () {
     changeImage();
   }, 5000);
 };
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("aboutMeShort").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setInterval(function () {
+      blink();
+    }, 400);
+  }
+}
+
+function blink() {
+  doc = document.getElementById("aboutMeShort");
+
+  if (doc.innerHTML[doc.innerHTML.length - 1] == "_") {
+    doc.innerHTML = doc.innerHTML.slice(0, doc.innerHTML.length - 1);
+  } else {
+    doc.innerHTML += "_";
+  }
+}
